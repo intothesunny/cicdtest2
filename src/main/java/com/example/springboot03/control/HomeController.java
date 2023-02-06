550c0ddb4ed42692cca569344b999dc8353d9337
KS <dagda@hanafos.com>
2023-02-07 01:47:05 +0900

    first commit

---
+++ src/main/java/com/example/springboot03/control/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.springboot03.control;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	// private Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		//log.info(">>>>>>>>>>>>>>>  : HomeController.home()  ");
+		
+		model.addAttribute("msg", "Hello spring boot JSP"); 
+		return "home";
+	}
+	@GetMapping("/welcome")
+	public String hi(Model model) {
+		model.addAttribute("name", "홍길동"); 
+		return "welcome";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2> home.jsp </h2>
+	<h3><a href="/welcome">환영합니다</a></h3>
+	
+	<!--   HomeController.hi() 
+				홍길동 <-- 
+			==> welcome.jsp
+			 
+		 	 홍길동님 환영합니다.
+	 -->
+</body>
+</html>
