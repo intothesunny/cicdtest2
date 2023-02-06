c72b566e9f1d3fe3f407a657f1e774abd18bc847
KS <dagda@hanafos.com>
2023-02-07 02:54:34 +0900

    4th commit

--- src/main/java/com/example/springboot03/control/HomeController.java
+++ src/main/java/com/example/springboot03/control/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	}
 	@GetMapping("/welcome")
 	public String hi(Model model) {
-		model.addAttribute("name", "이순신"); 
+		model.addAttribute("name", "이순신2"); 
 		return "welcome";
 	}
 }
