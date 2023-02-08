f8d5f39c4f03331b3c0ecd0af7fab321d4ad1ff6
KS <dagda@hanafos.com>
2023-02-08 13:44:25 +0900

    66

--- src/main/java/com/example/springboot03/control/HomeController.java
+++ src/main/java/com/example/springboot03/control/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	}
 	@GetMapping("/welcome")
 	public String hi(Model model) {
-		model.addAttribute("name", "HONG3"); 
+		model.addAttribute("name", "HONG66"); 
 		return "welcome";
 	}
 }
